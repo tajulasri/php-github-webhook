5c993d3cd9c11bb9ef093edb2303927685d7dac7
Tajul <mtajulasri@gmail.com>
2017-07-29 06:56:33 +0800

    set response on index

--- index.php
+++ index.php
@@ -7,6 +7,11 @@ use WebhookHandler\GithubWebhook;
 
 /**
  * expected usages
+ *
+ * expected hash : 9bfe272c5fa40b2fe0f6da2bd3caa6054429d79a
  */
 
-$webhook = (new GithubWebhook(new Request))->setCredentials(['secret_key' => 'testing'])->handle();
+$webhook = (new GithubWebhook(new Request))
+    ->setCredentials(['secret_key' => 'testing'])
+    ->handle()
+    ->getResponse();
